826067c4fb2144a71a7229d70689b9b7d5a37bc6
theus <tastelikeworms@gmail.com>
2025-04-22 18:44:48 -0300

    commit IMC

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [imc, setImc] = useState(null);
+  const [classificacao, setClassificacao] = useState('');
+
+  function calcularIMC() {
+    const pesoNum = parseFloat(peso);
+    const alturaNum = parseFloat(altura) / 100;
+    if (!pesoNum || !alturaNum) {
+      alert('Por favor, insira valores válidos!');
+      return;
+    }
+    const resultado = pesoNum / (alturaNum * alturaNum);
+    setImc(resultado.toFixed(2));
+    
+    if (resultado < 18.5) {
+      setClassificacao("Abaixo do peso");
+    }
+    else if (resultado >= 18.5 && resultado <= 24.9) {
+      setClassificacao("Peso saudável")
+    }
+    else if (resultado >= 25 && resultado <=29.9){
+      setClassificacao("Sobrepeso")
+    }
+    else {
+      setClassificacao("Obesidade")
+    }
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/logo.png')}
+      style={{ width: 200, height: 200, marginBottom: 20 }}
+      ></Image>
+      <Text style={styles.titulo}>Calculadora de IMC</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Peso (kg)'
+        keyboardType='numeric'
+        value={peso}
+        onChangeText={setPeso}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Altura (cm)'
+        keyboardType='numeric'
+        value={altura}
+        onChangeText={setAltura}
+      />
+      <Button title='Calcular' onPress={calcularIMC} />
+      {imc && (
+        <Text style={styles.resultado}>IMC: {imc} - {classificacao}</Text>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    padding: 20 
   },
+  titulo: { 
+    fontSize: 24, 
+    fontWeight: 'bold', 
+    marginBottom: 20 
+  },
+  input: 
+  { width: '80%', 
+    height: 40, 
+    borderWidth: 1, 
+    borderColor: '#ccc', 
+    marginBottom: 10, 
+    padding: 10, 
+    borderRadius: 5 
+  },
+  resultado: { 
+    marginTop: 20, 
+    fontSize: 18, 
+    fontWeight: 'bold' 
+  }
 });
